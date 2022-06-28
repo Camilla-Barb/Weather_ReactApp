954c04c24e6e8e39ce07d640f9754a0883e4ccb3
Camilla Barbieri <camilla.barbieri@accenture.com>
2022-06-28 15:19:50 +0200

    set style & introduction useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 const api = {
   key: "7d4ef5d23648c7c62acd3ccfa1a0a30b",
@@ -6,9 +6,42 @@ const api = {
 }
 
 function App() {
+  const [query, setQuery] = useState ('');
+  const [weather, setWeather] = useState ('')
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input 
+          type="text"
+          className="search-bar"
+          placeholder="Search..."
+          />
+        </div>
+        <div className="location-box">
+          <div className="lacation">New york City, US</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+          <div className="weather-box">
+            <div className="temp">
+              15°C
+            </div>
+            <div className="weather">Sunny</div>
+          </div>
+      </main>
     </div>
   );
 }
