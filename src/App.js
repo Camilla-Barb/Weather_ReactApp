bde6287e8b03d8774496297088b85d9b04c4c0a1
Camilla Barbieri <camilla.barbieri@accenture.com>
2022-06-28 14:32:07 +0200

    config work area & add bg photos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+const api = {
+  key: "7d4ef5d23648c7c62acd3ccfa1a0a30b",
+  base: "https://api.openweathermap.org/data/2.5"
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello</h1>
     </div>
   );
 }
